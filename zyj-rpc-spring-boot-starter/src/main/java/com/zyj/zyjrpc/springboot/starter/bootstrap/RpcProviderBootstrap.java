e361d76e6879caa77374466d163d120f1e30a0d9
Rhss <3567878896@qq.com>
2024-05-28 11:02:42 +0800

    项目完工

---
+++ zyj-rpc-spring-boot-starter/src/main/java/com/zyj/zyjrpc/springboot/starter/bootstrap/RpcProviderBootstrap.java
@@ -0,0 +1,66 @@
+package com.zyj.zyjrpc.springboot.starter.bootstrap;
+
+import com.zyj.zyjrpc.RpcApplication;
+import com.zyj.zyjrpc.config.RegistryConfig;
+import com.zyj.zyjrpc.config.RpcConfig;
+import com.zyj.zyjrpc.model.ServiceMetaInfo;
+import com.zyj.zyjrpc.registry.LocalRegistry;
+import com.zyj.zyjrpc.registry.Registry;
+import com.zyj.zyjrpc.registry.RegistryFactory;
+import com.zyj.zyjrpc.springboot.starter.annotation.RpcService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Rpc 服务提供者启动
+ */
+@Slf4j
+public class RpcProviderBootstrap implements BeanPostProcessor {
+
+    /**
+     * Bean 初始化后执行，注册服务
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> beanClass = bean.getClass();
+        RpcService rpcService = beanClass.getAnnotation(RpcService.class);
+        if (rpcService != null) {
+            // 需要注册服务
+            // 1. 获取服务基本信息
+            Class<?> interfaceClass = rpcService.interfaceClass();
+            // 默认值处理
+            if (interfaceClass == void.class) {
+                interfaceClass = beanClass.getInterfaces()[0];
+            }
+            String serviceName = interfaceClass.getName();
+            String serviceVersion = rpcService.serviceVersion();
+            // 2. 注册服务
+            // 本地注册
+            LocalRegistry.register(serviceName, beanClass);
+
+            // 全局配置
+            final RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+            // 注册服务到注册中心
+            RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+            Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+            serviceMetaInfo.setServiceName(serviceName);
+            serviceMetaInfo.setServiceVersion(serviceVersion);
+            serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
+            serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+            try {
+                registry.register(serviceMetaInfo);
+            } catch (Exception e) {
+                throw new RuntimeException(serviceName + " 服务注册失败", e);
+            }
+        }
+
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
